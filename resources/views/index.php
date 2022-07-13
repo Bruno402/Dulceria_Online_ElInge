35bea8082128171246678cf66b7c8bc561e4e9cc
America2105 <109228055+America2105@users.noreply.github.com>
2022-07-13 09:22:20 -0500

    Modificación 3

--- resources/views/index.php
+++ resources/views/index.php
@@ -46,9 +46,6 @@ if(!isset($_SESSION))
                             <a href="#" class="nav-link active">Catalogo</a>
                         </li>
 
-                        <li class="nav-item">
-                            <a href="#" class="nav-link">Contacto</a>
-                        </li>
                     </ul>
                     
                     <li>
